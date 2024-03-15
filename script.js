478732c72b74e5c2cdc4632268895322a334d9b7
felix <felix.fritz@hotmail.com>
2024-03-15 23:58:17 +0100

    màj memory

--- script.js
+++ script.js
@@ -27,14 +27,3 @@ wrapper.addEventListener('mousemove', function (e) {
     this.scrollLeft += startX - e.clientX
 })
 
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', () => {
-
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    })
-
-})
